a13d2d6ea3878758029b076802d73c63e7e77e93
TN De Alwis <tharudealwis@gmail.com>
2026-02-16 14:11:52 +0530

    db cont

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+const primsa = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+const connectDB = async () => {
+  try {
+    await primsa.$connect();
+    console.log("DB Connected via Prisma");
+  } catch (error) {
+    console.error(`Database connection error occured: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {};
